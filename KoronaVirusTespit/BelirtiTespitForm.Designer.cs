01feb30022dd23a792faf92715bd02228fdc7c11
Administrator <Administrator@WIN-4SOA9VBS7PE>
2022-03-23 14:19:14 +0300

    Add project files.

---
+++ KoronaVirusTespit/BelirtiTespitForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace KoronaVirusTespit
+{
+    partial class BelirtiTespitForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbAdi = new System.Windows.Forms.TextBox();
+            this.tbSoyadi = new System.Windows.Forms.TextBox();
+            this.ddlSehir = new System.Windows.Forms.ComboBox();
+            this.nudYas = new System.Windows.Forms.NumericUpDown();
+            this.rbErkek = new System.Windows.Forms.RadioButton();
+            this.rbKadin = new System.Windows.Forms.RadioButton();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbBogaz = new System.Windows.Forms.CheckBox();
+            this.cbOksuruk = new System.Windows.Forms.CheckBox();
+            this.cbAtes = new System.Windows.Forms.CheckBox();
+            this.tbSonuc = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYas)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyadı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Şehir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Yaş";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cinsiyet";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 287);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kontrol Tarihi";
+            // 
+            // tbAdi
+            // 
+            this.tbAdi.Location = new System.Drawing.Point(139, 40);
+            this.tbAdi.Name = "tbAdi";
+            this.tbAdi.Size = new System.Drawing.Size(250, 27);
+            this.tbAdi.TabIndex = 6;
+            // 
+            // tbSoyadi
+            // 
+            this.tbSoyadi.Location = new System.Drawing.Point(139, 81);
+            this.tbSoyadi.Name = "tbSoyadi";
+            this.tbSoyadi.Size = new System.Drawing.Size(250, 27);
+            this.tbSoyadi.TabIndex = 7;
+            // 
+            // ddlSehir
+            // 
+            this.ddlSehir.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlSehir.FormattingEnabled = true;
+            this.ddlSehir.Location = new System.Drawing.Point(139, 126);
+            this.ddlSehir.Name = "ddlSehir";
+            this.ddlSehir.Size = new System.Drawing.Size(250, 28);
+            this.ddlSehir.TabIndex = 8;
+            // 
+            // nudYas
+            // 
+            this.nudYas.Location = new System.Drawing.Point(139, 186);
+            this.nudYas.Name = "nudYas";
+            this.nudYas.Size = new System.Drawing.Size(250, 27);
+            this.nudYas.TabIndex = 9;
+            this.nudYas.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // rbErkek
+            // 
+            this.rbErkek.AutoSize = true;
+            this.rbErkek.Location = new System.Drawing.Point(281, 233);
+            this.rbErkek.Name = "rbErkek";
+            this.rbErkek.Size = new System.Drawing.Size(65, 24);
+            this.rbErkek.TabIndex = 10;
+            this.rbErkek.TabStop = true;
+            this.rbErkek.Text = "Erkek";
+            this.rbErkek.UseVisualStyleBackColor = true;
+            // 
+            // rbKadin
+            // 
+            this.rbKadin.AutoSize = true;
+            this.rbKadin.Location = new System.Drawing.Point(163, 233);
+            this.rbKadin.Name = "rbKadin";
+            this.rbKadin.Size = new System.Drawing.Size(68, 24);
+            this.rbKadin.TabIndex = 11;
+            this.rbKadin.TabStop = true;
+            this.rbKadin.Text = "Kadın";
+            this.rbKadin.UseVisualStyleBackColor = true;
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Location = new System.Drawing.Point(139, 282);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(250, 27);
+            this.dtpTarih.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.tbAdi);
+            this.groupBox1.Controls.Add(this.dtpTarih);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rbKadin);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.rbErkek);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.nudYas);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.ddlSehir);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbSoyadi);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 344);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kişisel Bilgiler";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.cbBogaz);
+            this.groupBox2.Controls.Add(this.cbOksuruk);
+            this.groupBox2.Controls.Add(this.cbAtes);
+            this.groupBox2.Location = new System.Drawing.Point(462, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(171, 257);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Belirtiler";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Tespit Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbBogaz
+            // 
+            this.cbBogaz.AutoSize = true;
+            this.cbBogaz.Location = new System.Drawing.Point(21, 128);
+            this.cbBogaz.Name = "cbBogaz";
+            this.cbBogaz.Size = new System.Drawing.Size(115, 24);
+            this.cbBogaz.TabIndex = 2;
+            this.cbBogaz.Text = "Boğaz Ağrısı";
+            this.cbBogaz.UseVisualStyleBackColor = true;
+            // 
+            // cbOksuruk
+            // 
+            this.cbOksuruk.AutoSize = true;
+            this.cbOksuruk.Location = new System.Drawing.Point(21, 84);
+            this.cbOksuruk.Name = "cbOksuruk";
+            this.cbOksuruk.Size = new System.Drawing.Size(83, 24);
+            this.cbOksuruk.TabIndex = 1;
+            this.cbOksuruk.Text = "Öksürük";
+            this.cbOksuruk.UseVisualStyleBackColor = true;
+            // 
+            // cbAtes
+            // 
+            this.cbAtes.AutoSize = true;
+            this.cbAtes.Location = new System.Drawing.Point(21, 45);
+            this.cbAtes.Name = "cbAtes";
+            this.cbAtes.Size = new System.Drawing.Size(60, 24);
+            this.cbAtes.TabIndex = 0;
+            this.cbAtes.Text = "Ateş";
+            this.cbAtes.UseVisualStyleBackColor = true;
+            // 
+            // tbSonuc
+            // 
+            this.tbSonuc.Location = new System.Drawing.Point(16, 34);
+            this.tbSonuc.Multiline = true;
+            this.tbSonuc.Name = "tbSonuc";
+            this.tbSonuc.Size = new System.Drawing.Size(599, 189);
+            this.tbSonuc.TabIndex = 16;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbSonuc);
+            this.groupBox3.Location = new System.Drawing.Point(12, 385);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(621, 246);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sonuç";
+            // 
+            // BelirtiTespitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(675, 699);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BelirtiTespitForm";
+            this.ShowIcon = false;
+            this.Text = "Belirti Tespit";
+            this.Load += new System.EventHandler(this.BelirtiTespitForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudYas)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox tbAdi;
+        private TextBox tbSoyadi;
+        private ComboBox ddlSehir;
+        private NumericUpDown nudYas;
+        private RadioButton rbErkek;
+        private RadioButton rbKadin;
+        private DateTimePicker dtpTarih;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button button1;
+        private CheckBox cbBogaz;
+        private CheckBox cbOksuruk;
+        private CheckBox cbAtes;
+        private TextBox tbSonuc;
+        private GroupBox groupBox3;
+    }
+}
---
+++ KoronaVirusTespit/BelirtiTespitForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoronaVirusTespit
+{
+    public partial class BelirtiTespitForm : Form
+    {
+        string[] _sehirler = new string[3]
+        {
+            "Ankara", "İstanbul", "İzmir"
+        };
+
+        string sonuc;
+        public BelirtiTespitForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BelirtiTespitForm_Load(object sender, EventArgs e)
+        {
+            this.Text = "Belirti Tespit";
+
+            //1.yöntem:
+            // ddlSehir.Items.Add("Ankara");
+            // ddlSehir.Items.Add("İstanbul");
+            // ddlSehir.Items.Add("izmir");
+
+            //2.yöntem
+            //foreach (string sehir in _sehirler)
+            //{
+            //    ddlSehir.Items.Add(sehir);
+            //}
+
+            //3.yöntem
+            ddlSehir.Items.AddRange(_sehirler);
+            ddlSehir.SelectedIndex = 0;
+
+            dtpTarih.Value = DateTime.Now;
+
+            rbKadin.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            KontrolEt()
+        }
+
+        private void KontrolEt()
+        {
+            sonuc = "";
+            if (rbKadin.Checked)
+            {
+                sonuc += "Bayan ";
+            }
+            else
+            {
+                sonuc += "Bay ";
+            }
+            sonuc += tbAdi.Text + " " + tbSoyadi.Text + "/r/n";
+            sonuc += "Şehriniz: " + ddlSehir.Text + "/r/n";
+            sonuc += "Yaşınız: " + nudYas.Value + "/r/n";
+            sonuc += "Kontrol tarihi: " + dtpTarih.Value.ToLongDateString() + "/r/n";
+
+            if (cbAtes.Checked || (cbOksuruk.Checked && cbBogaz.Checked))
+            {
+                sonuc += "Koronavirüs olabilirsiniz.";
+            }
+            else
+            {
+                sonuc += "Koronavirüs olmayabilirsiniz.";
+            }
+        }
+    }
+}
